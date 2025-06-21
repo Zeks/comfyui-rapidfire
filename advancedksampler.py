fa6c19c5e8b7534d74c329cf5dc665a421958a21
n.marchenko <enmarantispam@gmail.com>
2025-06-21 07:04:51 +0300

    working v1

--- advancedksampler.py
+++ advancedksampler.py
@@ -1,8 +1,11 @@
+
 from torch import Tensor
 from PIL import Image, ImageOps, ImageDraw, ImageFont
 from PIL.PngImagePlugin import PngInfo
 import numpy as np
 import torch
+
+import folder_paths
 
 import ast
 from pathlib import Path
@@ -13,6 +16,10 @@ import copy
 import subprocess
 import json
 import psutil
+
+import re
+import nodes as native
+from .adv_encode import advanced_encode #, advanced_encode_XL
 
 from comfy_extras.nodes_align_your_steps import AlignYourStepsScheduler
 from comfy_extras.nodes_gits import GITSScheduler
@@ -45,17 +52,136 @@ from comfy import samplers
 sys.path.append(custom_nodes_dir)
 
 
+
+class LoraTagLoader:
+    def __init__(self):
+        self.loaded_lora = None
+        self.tag_pattern = "\<[0-9a-zA-Z\:\_\-\.\s\/\(\)\\\\]+\>"
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "model": ("MODEL",),
+                              "clip": ("CLIP", ),
+                              "text": ("STRING", {"multiline": True}),
+                              }}
+    RETURN_TYPES = ("MODEL", "CLIP", "STRING")
+    RETURN_NAMES = ("MODEL", "CLIP", "STRING")
+    FUNCTION = "load_lora"
+
+    CATEGORY = "loaders"
+
+    def load_lora(self, model, clip, text):
+        # print(f"\nLoraTagLoader input text: { text }")
+
+        founds = re.findall(self.tag_pattern, text)
+        # print(f"\nfoound lora tags: { founds }")
+
+        if len(founds) < 1:
+            return (model, clip, text)
+
+        model_lora = model
+        clip_lora = clip
+        
+        lora_files = folder_paths.get_filename_list("loras")
+        for f in founds:
+            tag = f[1:-1]
+            pak = tag.split(":")
+            type = pak[0]
+            if type != 'lora':
+                continue
+            name = None
+            if len(pak) > 1 and len(pak[1]) > 0:
+                name = pak[1]
+            else:
+                continue
+            wModel = wClip = 0
+            try:
+                if len(pak) > 2 and len(pak[2]) > 0:
+                    wModel = float(pak[2])
+                    wClip = wModel
+                if len(pak) > 3 and len(pak[3]) > 0:
+                    wClip = float(pak[3])
+            except ValueError:
+                continue
+            if name == None:
+                continue
+            lora_name = None
+            for lora_file in lora_files:
+                if Path(lora_file).name.startswith(name) or lora_file.startswith(name):
+                    lora_name = lora_file
+                    break
+            if lora_name == None:
+                print(f"bypassed lora tag: { (type, name, wModel, wClip) } >> { lora_name }")
+                continue
+            print(f"detected lora tag: { (type, name, wModel, wClip) } >> { lora_name }")
+
+            lora_path = folder_paths.get_full_path("loras", lora_name)
+            lora = None
+            if self.loaded_lora is not None:
+                if self.loaded_lora[0] == lora_path:
+                    lora = self.loaded_lora[1]
+                else:
+                    temp = self.loaded_lora
+                    self.loaded_lora = None
+                    del temp
+
+            if lora is None:
+                lora = comfy.utils.load_torch_file(lora_path, safe_load=True)
+                self.loaded_lora = (lora_path, lora)
+
+            model_lora, clip_lora = comfy.sd.load_lora_for_models(model_lora, clip_lora, lora, wModel, wClip)
+
+        plain_prompt = re.sub(self.tag_pattern, "", text)
+        return (model_lora, clip_lora, plain_prompt)
+
+
+class AdvancedCLIPTextEncodeWithBreak:
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+            "text": ("STRING", {"multiline": True}),
+            "clip": ("CLIP", ),
+            "token_normalization": (["none", "mean", "length", "length+mean"],),
+            "weight_interpretation": (["comfy", "A1111", "compel", "comfy++" ,"down_weight"],),
+            }}
+    
+    RETURN_TYPES = ("CONDITIONING",)
+    FUNCTION = "encode"
+
+    CATEGORY = "conditioning/advanced"
+
+    def _encode(self, clip, text, token_normalization, weight_interpretation):
+        embeddings_final, pooled = advanced_encode(clip, text, token_normalization, weight_interpretation, w_max=1.0)
+        return ([[embeddings_final, {"pooled_output": pooled}]], )
+
+    def encode(self, clip, text, token_normalization, weight_interpretation):
+        prompts = re.split(r"\s*\bBREAK\b\s*", text) 
+        # encode first prompt fragment
+        prompt = prompts.pop(0)
+        # print(f"prompt: {prompt}")
+        out = self._encode(clip, prompt, token_normalization, weight_interpretation)
+        # encode and concatenate the rest of the prompt
+        for prompt in prompts:
+            # print(f"prompt: {prompt}")
+            cond_to = self._encode(clip, prompt, token_normalization, weight_interpretation)
+            out = native.ConditioningConcat.concat(self, cond_to[0], out[0])
+        return out
+
+
+
 class TwoModelAdvancedKsampler:
     @classmethod 
     def INPUT_TYPES(s):
         return {
             "required": { 
                 "model1": ("MODEL",),
-                "positive1": ("CONDITIONING",),
-                "negative1": ("CONDITIONING",),
+                "clip1": ("CLIP", ),
                 "model2": ("MODEL",),
-                "positive2": ("CONDITIONING",),
-                "negative2": ("CONDITIONING",),
+                "clip2": ("CLIP", ),
+                "positive": ("STRING",),
+                "negative": ("STRING",),
+                "lora_name": ("STRING",),
                 "rescaled_steps": ("INT",{"default": 8, "min": 0, "max": 100},),
                 "rescale_multiplier": (
                     "FLOAT",
@@ -98,6 +224,8 @@ class TwoModelAdvancedKsampler:
                 "steps1_start": ("INT", {"default": 0, "min": 0, "max": 100}),
                 "steps1_end": ("INT", {"default": 0, "min": 0, "max": 100}),
                 "start_steps2_shift": ("INT", {"default": 0, "min": -20, "max": 100}),
+                "token_normalization": (["none", "mean", "length", "length+mean"],),
+                "weight_interpretation": (["comfy", "A1111", "compel", "comfy++" ,"down_weight"],),
                 "latent_image": ("LATENT",),
             }
         }
@@ -108,25 +236,32 @@ class TwoModelAdvancedKsampler:
     CATEGORY = "sampling"
 
     def sample(self, 
-        model1, positive1, negative1,
-        model2, positive2, negative2, 
+        model1, clip1, 
+        model2, clip2,
+        positive, negative, lora_name,
         rescaled_steps,  rescale_multiplier,
         total_steps_original, total_steps_shift,
         noise_seed,
         sampler_name, scheduler,
         starting_cfg, cfg_shift,
         steps1_start, steps1_end, start_steps2_shift,
+        token_normalization,weight_interpretation,
         latent_image,
         **kwargs):
+            
+        model1, clip1, text1 = LoraTagLoader().load_lora(model1, clip1, lora_name)
+        model2, clip2, text2 = LoraTagLoader().load_lora(model2, clip2, lora_name)
+        
+        positive_conditioning1 = AdvancedCLIPTextEncodeWithBreak().encode(clip1, positive, token_normalization, weight_interpretation)
+        positive_conditioning2 = AdvancedCLIPTextEncodeWithBreak().encode(clip2, positive, token_normalization, weight_interpretation)
+        negative_conditioning1 = AdvancedCLIPTextEncodeWithBreak().encode(clip1, negative, token_normalization, weight_interpretation)
+        negative_conditioning2 = AdvancedCLIPTextEncodeWithBreak().encode(clip2, negative, token_normalization, weight_interpretation)
         
         # Debug print all input parameters
         print("\n=== TwoModelAdvancedKsampler Debug Information ===")
         print(f"Model1: {type(model1)}")
         print(f"Model2: {type(model2)}")
-        print(f"Positive1: {len(positive1)} elements")
-        print(f"Negative1: {len(negative1)} elements")
-        print(f"Positive2: {len(positive2)} elements")
-        print(f"Negative2: {len(negative2)} elements")
+
         print(f"Total Steps Original: {total_steps_original}")
         print(f"Total Steps Shift: {total_steps_shift} (New total: {total_steps_original + total_steps_shift})")
         print(f"Noise Seed: {noise_seed}")
@@ -153,8 +288,8 @@ class TwoModelAdvancedKsampler:
                     starting_cfg, 
                     sampler_name, 
                     scheduler,
-                    positive1, 
-                    negative1, 
+                    positive_conditioning1[0], 
+                    negative_conditioning1[0], 
                     latent_image, 
                     steps1_start, 
                     rescaled_steps,
@@ -179,8 +314,8 @@ class TwoModelAdvancedKsampler:
                 starting_cfg, 
                 sampler_name, 
                 scheduler,
-                positive1, 
-                negative1, 
+                positive_conditioning1[0], 
+                negative_conditioning1[0], 
                 samples[0], 
                 actual_steps, 
                 steps1_end,
@@ -214,8 +349,8 @@ class TwoModelAdvancedKsampler:
                 new_cfg, 
                 sampler_name, 
                 scheduler,
-                positive2, 
-                negative2, 
+                positive_conditioning2[0], 
+                negative_conditioning2[0], 
                 samples[0], 
                 model2_start_step, 
                 model2_end_step,
